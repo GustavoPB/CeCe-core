d20f2284760987b1cbfc40e4ffab644129f5aba1
Jiří Fatka <fatkaj@ntis.zcu.cz>
2016-09-06 14:02:38 +0200

    Improved render::Color tests.

--- cece/render/test/ColorTest.cpp
+++ cece/render/test/ColorTest.cpp
@@ -44,10 +44,10 @@ TEST(ColorTest, parse)
         Color color;
         iss >> color;
 
-        EXPECT_EQ(1.f, color.getRed());
-        EXPECT_EQ(1.f, color.getGreen());
-        EXPECT_EQ(1.f, color.getBlue());
-        EXPECT_EQ(1.f, color.getAlpha());
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
     }
 
     {
@@ -55,10 +55,254 @@ TEST(ColorTest, parse)
         Color color;
         iss >> color;
 
-        EXPECT_EQ(0.f, color.getRed());
-        EXPECT_EQ(0.f, color.getGreen());
-        EXPECT_EQ(0.f, color.getBlue());
-        EXPECT_EQ(0.f, color.getAlpha());
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+
+    {
+        InStringStream iss("#FF0000");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+
+    {
+        InStringStream iss("#00FF00");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+
+    {
+        InStringStream iss("#0000FF");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+
+    {
+        InStringStream iss("#00000000");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(0.f, color.getAlpha());
+    }
+
+
+    {
+        InStringStream iss("#FFFFFF00");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(0.f, color.getAlpha());
+    }
+}
+
+/* ************************************************************************ */
+
+TEST(ColorTest, parseNamed)
+{
+    {
+        InStringStream iss("white");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("black");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("red");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("green");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("blue");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("yellow");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(0.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("cyan");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(0.f, color.getRed());
+        EXPECT_FLOAT_EQ(1.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+
+    {
+        InStringStream iss("magenta");
+        Color color;
+        iss >> color;
+
+        EXPECT_FLOAT_EQ(1.f, color.getRed());
+        EXPECT_FLOAT_EQ(0.f, color.getGreen());
+        EXPECT_FLOAT_EQ(1.f, color.getBlue());
+        EXPECT_FLOAT_EQ(1.f, color.getAlpha());
+    }
+}
+
+/* ************************************************************************ */
+
+TEST(ColorTest, printHex)
+{
+    {
+        OutStringStream oss;
+        Color color = Color::fromUchar(0xFF, 0xFF, 0xF0);
+        oss << color;
+
+        EXPECT_EQ("#FFFFF0FF", oss.str());
+    }
+
+
+    {
+        OutStringStream oss;
+        Color color = Color::fromUchar(0xA0, 0x08, 0xFB, 0x38);
+        oss << color;
+
+        EXPECT_EQ("#A008FB38", oss.str());
+    }
+}
+
+/* ************************************************************************ */
+
+TEST(ColorTest, printNamed)
+{
+    {
+        OutStringStream oss;
+        Color color = colors::WHITE;
+        oss << color;
+
+        EXPECT_EQ("white", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::BLACK;
+        oss << color;
+
+        EXPECT_EQ("black", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::RED;
+        oss << color;
+
+        EXPECT_EQ("red", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::GREEN;
+        oss << color;
+
+        EXPECT_EQ("green", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::BLUE;
+        oss << color;
+
+        EXPECT_EQ("blue", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::YELLOW;
+        oss << color;
+
+        EXPECT_EQ("yellow", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::CYAN;
+        oss << color;
+
+        EXPECT_EQ("cyan", oss.str());
+    }
+
+    {
+        OutStringStream oss;
+        Color color = colors::MAGENTA;
+        oss << color;
+
+        EXPECT_EQ("magenta", oss.str());
     }
 }
 
